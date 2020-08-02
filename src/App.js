f8e27e409c9bc0ffae619f462196a615c7478eaa
beatorizu <betrizaf@gmail.com>
2020-08-01 23:37:14 -0300

    feat: Add banner

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React from 'react';
 
 import Menu from "./components/Menu";
 import dadosIniciais from "./data/dados_iniciais.json";
+import BannerMain from './components/BannerMain';
 
 function App() {
   return (
-    <div className="App">
+    <div style={{ background: "#141414" }}>
       <Menu />
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={
+          "O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"
+        }
+      />
     </div>
   );
 }
