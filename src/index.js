47d790d8ddfa8ad5ac0a8023edf21edddae286a5
beatorizu <betrizaf@gmail.com>
2020-08-02 17:03:38 -0300

    feat: Add not found

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './pages/Home';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import NotFound from './pages/NotFound';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" component={Home} exact />
+      <Route component={NotFound} />
     </Switch>
   </BrowserRouter>,
   document.getElementById('root')
---
+++ src/pages/NotFound/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import NotFoundImg from "../../assets/img/NotFound.gif";
+
+function NotFound() {
+  return <img src={NotFoundImg} alt="Not found" className="NotFound" />;
+}
+
+export default NotFound;
