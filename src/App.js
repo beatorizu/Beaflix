bba137fc819b24bec48aaec39a93456fa3b84b2d
beatorizu <betrizaf@gmail.com>
2020-08-01 23:44:26 -0300

    feat: Add footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Menu from "./components/Menu";
 import dadosIniciais from "./data/dados_iniciais.json";
 import BannerMain from './components/BannerMain';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -16,6 +17,8 @@ function App() {
           "O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"
         }
       />
+
+      <Footer />
     </div>
   );
 }
