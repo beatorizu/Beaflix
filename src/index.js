6496c4c1ac546b3648d1e1ef5b68a17838e846c7
beatorizu <betrizaf@gmail.com>
2020-08-02 16:16:58 -0300

    feat: Add home route

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
