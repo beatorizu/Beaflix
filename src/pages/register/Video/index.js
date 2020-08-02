8e6790173241f8b449a54a357e215d9db82b09d2
beatorizu <betrizaf@gmail.com>
2020-08-02 18:51:18 -0300

    update: Footer in footer

--- src/pages/register/Video/index.js
+++ src/pages/register/Video/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import DefaultPage from "../../../components/DefaultPage";
+import { Link } from "react-router-dom";
 
 function RegisterVideo() {
   return (
     <DefaultPage>
-      Register Video Page
+      <h1>Cadastro de Vídeo</h1>
+      <Link to='/register/category'>
+        Cadastrar Categoria
+      </Link>
     </DefaultPage>
   );
 }
